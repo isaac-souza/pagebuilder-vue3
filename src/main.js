263527b3358cc2ff7fad21a2aed606aa54c757f4
Isaac Souza <isaacsouza17@gmail.com>
2022-01-23 08:15:50 -0300

    Vue router setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Router from './router'
 import './index.css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(Router)
+    .mount('#app')
