143f3c947f0732eb1612d158684f8c024a18e50f
Isaac Souza <isaacsouza17@gmail.com>
2022-01-23 08:54:36 -0300

    Nice looking auth layout and pages

--- src/Pages/Auth/Login.vue
+++ src/Pages/Auth/Login.vue
@@ -1,16 +1,26 @@
 <template>
     <AuthLayout>
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Username</span>
-            </label>
-            <input type="text" placeholder="username" class="input input-bordered">
+        <div class="text-center">
+            <h1 class="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
+                Login
+            </h1>
+            <div class="py-2 text-left">
+                <input type="email" class="input input-bordered w-full" placeholder="Email" />
+            </div>
+            <div class="py-2 text-left">
+                <input type="password" class="input input-bordered w-full" placeholder="Password" />
+            </div>
+            <div class="py-2">
+                <button type="submit" class="btn btn-primary btn-block">
+                    Login
+                </button>
+            </div>
         </div>
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Username</span>
-            </label>
-            <input type="text" placeholder="username" class="input input-bordered">
+        <div class="text-center mt-12">
+            <span>
+                Don't have an account?
+            </span>
+            <router-link to="/register" class="text-md text-indigo-600 underline font-semibold hover:text-indigo-800">Create One</router-link>
         </div>
     </AuthLayout>
 </template>
--- src/Pages/Auth/Register.vue
+++ src/Pages/Auth/Register.vue
@@ -1,30 +1,35 @@
 <template>
     <AuthLayout>
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Name</span>
-            </label>
-            <input type="text" placeholder="username" class="input input-bordered">
+        <div class="text-center">
+            <h1 class="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
+                Create account
+            </h1>
+            <div class="py-2 text-left">
+                <input type="text" class="input input-bordered w-full" placeholder="Name"/>
+            </div>
+            <div class="py-2 text-left">
+                <input type="email" class="input input-bordered w-full" placeholder="Email"/>
+            </div>
+            <div class="py-2 text-left">
+                <input type="password" class="input input-bordered w-full" placeholder="Password"/>
+            </div>
+            <div class="py-2 text-left">
+                <input type="password" class="input input-bordered w-full" placeholder="Password confirmation"/>
+            </div>
+            <div class="py-2">
+                <button type="submit" class="btn btn-primary btn-block">
+                    Create account
+                </button>
+            </div>
         </div>
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Email</span>
-            </label>
-            <input type="text" placeholder="username" class="input input-bordered">
-        </div>
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Password</span>
-            </label>
-            <input type="text" placeholder="username" class="input input-bordered">
-        </div>
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Password confirmation</span>
-            </label>
-            <input type="text" placeholder="username" class="input input-bordered">
+        <div class="text-center mt-12">
+            <span>
+                Already have an account?
+            </span>
+            <router-link to="/login" class="text-md text-indigo-600 underline font-semibold hover:text-indigo-800">Sing in</router-link>
         </div>
     </AuthLayout>
+    <div class="pb-12"></div>
 </template>
 
 <script>
