f62cef5cb705cd27e44bd315b6f80b0070feddff
Isaac Souza <isaacsouza17@gmail.com>
2022-01-23 08:17:45 -0300

    Component to render page blocks

--- src/Components/Blocks/BlockRenderer.vue
+++ src/Components/Blocks/BlockRenderer.vue
@@ -1,43 +1,89 @@
 <template>
     <div class="relative">
-        <ImageLeft v-if="id == 1"/>
-        <FormLeft v-if="id == 2"/>
-        <VideoCenter v-if="id == 3"/>
+        <div class="pl-14">
+            <ImageLeft v-if="block.id == 1" :block="block"/>
+            <FormLeft v-if="block.id == 2" :block="block"/>
+            <VideoCenter v-if="block.id == 3" :block="block"/>
+        </div>
 
-        <div class="absolute top-1 left-1 flex">
-            <div class="btn btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-6 w-6 opacity-60 bi bi-arrows-move" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z"/></svg>
+        <div class="absolute top-0 left-0 flex flex-col py-2 space-y-2 text-white bg-gray-700 rounded-md">
+            <div class="btn btn-sm btn-ghost handle">
+                <Icon name="move"/>
             </div>
-            <div class="btn btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-6 w-6 opacity-60 bi bi-pencil-square" viewBox="0 0 16 16"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>
+            <div class="flex flex-col items-center justify-center drawer-content">
+                <label @click="toggleDrawer()" for="my-drawer" class="btn btn-sm btn-ghost drawer-button">
+                    <Icon name="edit"/>
+                </label>
             </div>
-            <div class="btn btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-6 w-6 opacity-60 bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>
+            <div @click="handleDeleteEvent()" class="btn btn-sm btn-ghost text-red-600">
+                <Icon name="trash"/>
             </div>
         </div>
     </div>
+
+    <teleport to="body">
+        <div v-if="showDrawer" class="fixed top-0 left-0">
+            <input id="my-drawer" type="checkbox" class="drawer-toggle">
+            <div class="drawer-side h-screen">
+                <label for="my-drawer" class="drawer-overlay"></label>
+                <ul class="menu p-4 overflow-y-auto w-72 bg-base-100 text-base-content">
+                    <button @click="showDrawer = false" class="btn btn-sm btn-active mt-2">Fechar</button>
+                    
+                    <p class="my-4 font-bold uppercase">Opções do bloco</p>
+                    
+                    <ImageLeftEditForm v-if="block.id == 1" v-model="block"/>
+
+                    <button @click="showDrawer = false" class="btn btn-sm btn-active mt-2">Fechar</button>
+                </ul>
+            </div>
+        </div>
+    </teleport>
 </template>
 
 <script>
+    import { ref } from 'vue'
+    import Icon from './../Icon.vue'
+    import Modal from './../Modal.vue'
     import ImageLeft from './Heroes/ImageLeft.vue'
+    import ImageLeftEditForm from './Heroes/ImageLeftEditForm.vue'
     import FormLeft from './Heroes/FormLeft.vue'
     import VideoCenter from './Heroes/VideoCenter.vue'
 
     export default {
         name: 'BlockRenderer',
+        emits: ['delete'],
         components: {
+            Icon,
+            Modal,
             ImageLeft,
+            ImageLeftEditForm,
             FormLeft,
             VideoCenter,
         },
         props: {
-            id: {
-                type: Number,
+            block: {
+                type: Object,
                 required: true
             }
         },
-        setup() {
-            
+        setup(props, context) {
+            const showEditModal = ref(false)
+
+            const toggleEditModal = () => {
+                showEditModal.value = !showEditModal.value
+            }
+
+            const handleDeleteEvent = () => {
+                context.emit('delete', props.block)
+            }
+
+            const showDrawer = ref(false)
+
+            const toggleDrawer = () => {
+                showDrawer.value = !showDrawer.value
+            }
+
+            return { handleDeleteEvent, showEditModal, toggleEditModal, showDrawer, toggleDrawer }
         },
     }
 </script>
