a2f0f5116fa5801272e9857b13e9d70c7dcb8169
Isaac Souza <isaacsouza17@gmail.com>
2022-01-23 17:25:46 -0300

    Login implementation fully functional

--- src/Pages/Auth/Login.vue
+++ src/Pages/Auth/Login.vue
@@ -1,17 +1,23 @@
 <template>
     <AuthLayout>
-        <div class="text-center">
+        <div class="text-center space-y-2">
             <h1 class="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
                 Login
             </h1>
             <div class="py-2 text-left">
-                <input type="email" class="input input-bordered w-full" placeholder="Email" />
+                <input v-model="form.email" type="email" class="input input-bordered w-full" placeholder="Email" />
             </div>
             <div class="py-2 text-left">
-                <input type="password" class="input input-bordered w-full" placeholder="Password" />
+                <input v-model="form.password" type="password" class="input input-bordered w-full" placeholder="Password" />
+            </div>
+            <div v-if="error" class="text-red-500 text-sm text-center w-full p-4 bg-red-100 rounded-md">
+                Authentication error, please try again.
             </div>
             <div class="py-2">
-                <button type="submit" class="btn btn-primary btn-block">
+                <button v-if="processing" disabled class="btn btn-primary btn-block loading disabled">
+                    Checking credentials...
+                </button>
+                <button v-else @click="submit()" type="submit" class="btn btn-primary btn-block">
                     Login
                 </button>
             </div>
@@ -26,15 +32,44 @@
 </template>
 
 <script>
+    import { defineComponent, ref } from 'vue'
+    import { useRouter } from 'vue-router'
     import AuthLayout from '../../Layouts/Auth.vue'
+    import Api from '../../Utils/api'
 
-    export default {
+    export default defineComponent({
         name: 'Login',
         components: {
             AuthLayout,
         },
         setup() {
-            
+            const router = useRouter()
+
+            const error = ref(false)
+            const processing = ref(false)
+
+            const form = ref({
+                email: 'justyn57@example.net',
+                password: 'password',
+            })
+
+            const submit = async () => {
+                processing.value = true
+                error.value = false
+
+                const result = await Api.login(form.value)
+
+                if(result) {
+                    router.push({name: 'Dashboard'})
+                }
+                else {
+                    error.value = true
+                }
+
+                processing.value = false
+            }
+
+            return { form, submit, error, processing }
         },
-    }
+    })
 </script>
