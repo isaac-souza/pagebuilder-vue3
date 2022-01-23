bebf8f1a41557472249e0d9b33a422325b0fc185
Isaac Souza <isaacsouza17@gmail.com>
2022-01-23 17:25:18 -0300

    Updated marketing layout with navbar

---
+++ src/Layouts/Marketing.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="navbar mb-2 shadow-lg">
+        <div class="flex-none px-2 mx-2">
+            <span class="text-lg font-bold">
+                EZ LandingPage
+            </span>
+        </div> 
+        <div class="flex-1 px-2 mx-2">
+            <div class="items-stretch hidden lg:flex">
+                <a class="btn btn-ghost rounded-btn">
+                    Home
+                </a>
+                <a class="btn btn-ghost rounded-btn">
+                    Pricing
+                </a>
+                <a class="btn btn-ghost rounded-btn">
+                    Contact
+                </a>
+            </div>
+        </div> 
+        <div class="flex justify-end gap-x-2">
+            <router-link to="/login" class="btn btn-ghost">
+                Login
+            </router-link>
+            <router-link to="/register" class="btn btn-primary">
+                Register
+            </router-link>
+        </div>
+    </div>
+
+    <slot/>
+</template>
