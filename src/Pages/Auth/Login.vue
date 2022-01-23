7ee08f32a69fc7bacb24de93159826fe7ca78b80
Isaac Souza <isaacsouza17@gmail.com>
2022-01-23 08:16:48 -0300

    Authentication pages

--- src/Pages/Auth/Login.vue
+++ src/Pages/Auth/Login.vue
@@ -1,23 +1,28 @@
 <template>
-    
-    <div class="form-control">
-        <label class="label">
-            <span class="label-text">Username</span>
-        </label> 
-        <input type="text" placeholder="username" class="input input-bordered">
-    </div>
-
-    <div class="form-control">
-        <label class="label">
-            <span class="label-text">Username</span>
-        </label> 
-        <input type="text" placeholder="username" class="input input-bordered">
-    </div>
-
+    <AuthLayout>
+        <div class="form-control">
+            <label class="label">
+                <span class="label-text">Username</span>
+            </label>
+            <input type="text" placeholder="username" class="input input-bordered">
+        </div>
+        <div class="form-control">
+            <label class="label">
+                <span class="label-text">Username</span>
+            </label>
+            <input type="text" placeholder="username" class="input input-bordered">
+        </div>
+    </AuthLayout>
 </template>
 
 <script>
+    import AuthLayout from '../../Layouts/Auth.vue'
+
     export default {
+        name: 'Login',
+        components: {
+            AuthLayout,
+        },
         setup() {
             
         },
---
+++ src/Pages/Auth/Register.vue
@@ -0,0 +1,42 @@
+<template>
+    <AuthLayout>
+        <div class="form-control">
+            <label class="label">
+                <span class="label-text">Name</span>
+            </label>
+            <input type="text" placeholder="username" class="input input-bordered">
+        </div>
+        <div class="form-control">
+            <label class="label">
+                <span class="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="username" class="input input-bordered">
+        </div>
+        <div class="form-control">
+            <label class="label">
+                <span class="label-text">Password</span>
+            </label>
+            <input type="text" placeholder="username" class="input input-bordered">
+        </div>
+        <div class="form-control">
+            <label class="label">
+                <span class="label-text">Password confirmation</span>
+            </label>
+            <input type="text" placeholder="username" class="input input-bordered">
+        </div>
+    </AuthLayout>
+</template>
+
+<script>
+    import AuthLayout from '../../Layouts/Auth.vue'
+
+    export default {
+        name: 'Login',
+        components: {
+            AuthLayout,
+        },
+        setup() {
+            
+        },
+    }
+</script>
