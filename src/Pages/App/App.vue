92695db41cc8f9c27d7e26dea27cd9022aad0fe0
Isaac Souza <isaacsouza17@gmail.com>
2022-01-23 17:26:36 -0300

    Base components to support App children routes

---
+++ src/Pages/App/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <router-view/>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        setup() {
+            
+        },
+    })
+</script>
