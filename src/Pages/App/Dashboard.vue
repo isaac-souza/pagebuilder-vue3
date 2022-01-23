70b18ed15c0bfa3e615b5b87fe02e152fb57fdc5
Isaac Souza <isaacsouza17@gmail.com>
2022-01-23 17:26:46 -0300

    App dashboard component

---
+++ src/Pages/App/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <AppLayout>
+        App
+    </AppLayout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import AppLayout from '../../Layouts/App.vue'
+
+export default defineComponent({
+    name: 'Dashboard',
+    components: {
+        AppLayout,
+    },
+    setup() {
+        
+    },
+})
+</script>
+
