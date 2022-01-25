9dfd06c4acca21e833bec16e87050ce63ea5af9a
Isaac Souza <isaacsouza17@gmail.com>
2022-01-25 18:51:33 -0300

    Fix button link

--- src/Components/Blocks/Heroes/ImageLeft.vue
+++ src/Components/Blocks/Heroes/ImageLeft.vue
@@ -3,7 +3,7 @@
         <div class="col-span-12 md:col-span-7 flex flex-col items-start justify-center p-8 space-y-4">
             <h1 class="text-3xl font-black">{{ block.data.title }}</h1>
             <h2 class="text-xl text-gray-700">{{ block.data.subtitle }}</h2>
-            <a :href="buttonLink" class="btn btn-error btn-lg">{{ block.data.buttonText }}</a>
+            <a :href="block.data.buttonLink" class="btn btn-error btn-lg">{{ block.data.buttonText }}</a>
         </div>
         <div class="col-span-12 md:col-span-5 p-8">
             <img :src="block.data.imageUrl" class="">
