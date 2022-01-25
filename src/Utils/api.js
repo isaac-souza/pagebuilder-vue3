a7538ed678cba76400ff1e542200ec7a6806a4f6
Isaac Souza <isaacsouza17@gmail.com>
2022-01-25 18:55:23 -0300

    Added landingpage endpoints

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -7,7 +7,6 @@ const Api = {
     login: async (credentials) => {
         try {
             await axios.get('/sanctum/csrf-cookie')
-
             const response = await axios.post('/login', credentials)
 
             if(response.status == 200) {
@@ -36,10 +35,7 @@ const Api = {
     register: async (data) => {
         try {
             await axios.get('/sanctum/csrf-cookie')
-
             const response = await axios.post('/register', data)
-
-            console.log(response)
 
             if(response.status == 200) {
                 return true
@@ -53,13 +49,65 @@ const Api = {
     },
 
     isAuthenticated: async () => {
-        await axios.get('/sanctum/csrf-cookie')
-            
         try {
-            const response = await axios.get('/api/auth-check')
+            await axios.get('/sanctum/csrf-cookie')
+            const response = await axios.get('/api/v1/auth/check')
             return response.data.authenticated
         } catch (error) {
             return false
+        }
+    },
+
+    fetchLandingPages: async () => {
+        try {
+            await axios.get('/sanctum/csrf-cookie')
+            const response = await axios.get('/api/v1/landing-pages')
+            return response.data.data
+        } catch (error) {
+            return null
+        }
+    },
+
+    fetchLandingPage: async (uuid) => {
+        try {
+            await axios.get('/sanctum/csrf-cookie')
+            const response = await axios.get('/api/v1/landing-pages/' + uuid)
+            return response.data.data
+        } catch (error) {
+            return null
+        }
+    },
+
+    fetchAccount: async () => {
+        await axios.get('/sanctum/csrf-cookie')
+            
+        try {
+            const response = await axios.get('/api/v1/auth/account')
+            return response.data.data
+        } catch (error) {
+            return null
+        }
+    },
+
+    updateLandingPage: async (uuid, data) => {
+        await axios.get('/sanctum/csrf-cookie')
+            
+        try {
+            const response = await axios.put('/api/v1/landing-pages/' + uuid, data)
+            return response.data
+        } catch (error) {
+            return null
+        }
+    },
+
+    updateDraft: async (uuid, data) => {
+        await axios.get('/sanctum/csrf-cookie')
+            
+        try {
+            const response = await axios.put('/api/v1/landing-pages/' + uuid + '/draft', data)
+            return response.data
+        } catch (error) {
+            return null
         }
     }
 }
