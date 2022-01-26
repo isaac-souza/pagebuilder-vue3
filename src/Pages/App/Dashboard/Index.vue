507ca03d1ea7db9ae68424e113fd319e47ec6792
Isaac Souza <isaacsouza17@gmail.com>
2022-01-25 22:50:33 -0300

    Added pewview link in the dashboard

--- src/Pages/App/Dashboard/Index.vue
+++ src/Pages/App/Dashboard/Index.vue
@@ -21,7 +21,7 @@
                     <div class="card-body p-4">
                         <h2 class="card-title text-base font-normal">{{ landingPage.name }}</h2> 
                         <div class="justify-end card-actions m-0">
-                            <a :href="'/' + landingPage.slug" target="_blank" class="btn btn-square btn-sm btn-outline btn-primary">
+                            <a :href="'/app/landing-pages/' + landingPage.uuid + '/preview'" target="_blank" class="btn btn-square btn-sm btn-outline btn-primary">
                                 <Icon name="external-link" styles="w-3 h-3"/>
                             </a>
                             <router-link :to="'/app/landing-pages/' + landingPage.uuid + '/builder'" class="btn btn-square btn-sm btn-outline btn-primary">
