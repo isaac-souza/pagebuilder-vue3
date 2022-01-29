c3069038fd1539e9f36fb9f7a3bf303d73d98bc9
Isaac Souza <isaacsouza17@gmail.com>
2022-01-29 18:13:59 -0300

    Lil helper to show the current breakpoint

---
+++ src/Components/BreakPointHelper.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="fixed bottom-2 right-2 text-gray-900">
+        <span class="bg-green-300 rounded-xl p-4 block sm:hidden">XS</span>
+        <span class="bg-blue-300 rounded-xl p-4 hidden sm:block md:hidden">SM</span>
+        <span class="bg-purple-300 rounded-xl p-4 hidden md:block lg:hidden">MD</span>
+        <span class="bg-yellow-300 rounded-xl p-4 hidden lg:block xl:hidden">LG</span>
+        <span class="bg-red-300 rounded-xl p-4 hidden xl:block 2xl:hidden">XL</span>
+        <span class="bg-gray-300 rounded-xl p-4 hidden 2xl:block">2XL</span>
+    </div>
+</template>
