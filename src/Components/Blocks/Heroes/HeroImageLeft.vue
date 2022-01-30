d3dc4e9b4845d4dc0aba71bc604bb0b091238749
Isaac Souza <isaacsouza17@gmail.com>
2022-01-30 10:07:39 -0300

    Added dark mode to hero image left block

--- src/Components/Blocks/Heroes/HeroImageLeft.vue
+++ src/Components/Blocks/Heroes/HeroImageLeft.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-white">
+    <div :class="{'bg-gray-900 text-gray-100': block.data.darkMode, 'bg-white': !block.data.darkMode}">
         <div class="max-w-6xl mx-auto">
             <div :class="{'py-6 md:py-12': first, 'py-12 md:py-24': !first}" class="px-4 md:px-8">
                 <div class="grid grid-cols-12 space-y-8">
@@ -7,14 +7,14 @@
                         <h1 class="text-3xl md:text-4xl font-bold">
                             {{ block.data.title }}
                         </h1>
-                        <h2 class="text-xl md:text-2xl text-gray-700">
+                        <h2 class="text-xl md:text-2xl">
                             {{ block.data.subtitle }}
                         </h2>
                         <a :href="block.data.buttonLink" target="_blank" class="btn btn-error btn-lg px-12 text-lg md:text-xl mx-auto md:ml-0">
                             {{ block.data.buttonText }}
                         </a>
                     </div>
-                    <div class="col-span-12 md:col-span-5 order-1 md:order-2">
+                    <div class="col-span-12 md:col-span-5 order-1 md:order-2 pb-12 md:pb-0">
                         <img :src="block.data.imageUrl" class="max-w-xs max-h-96 block mx-auto" loading="lazy">
                     </div>
                 </div>
