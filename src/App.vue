79d68a09ab665af949804ab8da33eca8c50a3d36
Isaac Souza <isaacsouza17@gmail.com>
2022-02-02 19:46:09 -0300

    Animation speed decrease

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 <style scoped>
     .fade-enter-active,
     .fade-leave-active {
-        transition: opacity 0.5s ease-in-out;
+        transition: opacity 0.2s ease-in-out;
     }
 
     .fade-enter-from,
