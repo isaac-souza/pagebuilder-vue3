8142b6abb4d0e48e3f782a8f8165361ffb98bbbd
Isaac Souza <isaacsouza17@gmail.com>
2022-02-02 12:54:49 -0300

    Updated actions and mutations constants

--- src/Utils/store.js
+++ src/Utils/store.js
@@ -2,9 +2,12 @@ import { createStore } from 'vuex';
 import Api from './api'
 
 import {
-    SET_LANDING_PAGES,
-    ADD_LANDING_PAGE,
-    DELETE_LANDING_PAGE,
+    MUTATION_SET_LANDING_PAGES,
+    MUTATION_ADD_LANDING_PAGE,
+    MUTATION_DELETE_LANDING_PAGE,
+    MUTATION_UPDATE_LANDING_PAGE,
+
+    MUTATION_SET_ALERT,
 } from './mutation-types'
 
 import {
@@ -12,27 +15,46 @@ import {
     ACTION_GET_LANDING_PAGE_BY_SLUG,
     ACTION_DELETE_LANDING_PAGE,
     ACTION_CREATE_LANDING_PAGE,
+    ACTION_UPDATE_DRAFT,
+    ACTION_UPDATE_PAGES,
+
+    ACTION_DISMISS_ALERT,
+    ACTION_SHOW_ALERT,
 } from './action-types'
 
 export default createStore({
     state() {
         return {
-            landingPages: null
+            landingPages: null,
+            alert: {
+                show: false,
+                type: null,
+                message: null
+            }
         }
     },
 
     mutations: {
-        [SET_LANDING_PAGES](state, payload) {
+        [MUTATION_SET_LANDING_PAGES](state, payload) {
             state.landingPages = payload.landingPages
         },
 
-        [ADD_LANDING_PAGE](state, payload) {
+        [MUTATION_ADD_LANDING_PAGE](state, payload) {
             state.landingPages.push(payload.landingPage)
         },
 
-        [DELETE_LANDING_PAGE](state, payload) {
+        [MUTATION_DELETE_LANDING_PAGE](state, payload) {
             let index = state.landingPages.findIndex(landingPage => landingPage.uuid == payload.uuid)
             state.landingPages.splice(index, 1)
+        },
+
+        [MUTATION_UPDATE_LANDING_PAGE](state, payload) {
+            let index = state.landingPages.findIndex(landingPage => landingPage.uuid == payload.landingPage.uuid)
+            state.landingPages[index] = payload.landingPage
+        },
+
+        [MUTATION_SET_ALERT](state, payload) {
+            state.alert = payload.alert
         },
     },
 
@@ -52,7 +74,7 @@ export default createStore({
                 if(context.state.landingPages == null) {
                     Api.fetchLandingPages()
                         .then(response => {
-                            context.commit(SET_LANDING_PAGES, { landingPages: response.data })
+                            context.commit(MUTATION_SET_LANDING_PAGES, { landingPages: response.data })
                             resolve(response)
                         })
                         .catch(error => {
@@ -62,12 +84,17 @@ export default createStore({
             })
         },
 
-        [ACTION_DELETE_LANDING_PAGE]: async (context, uuid) => {
-            const result = await Api.deleteLandingPage(uuid)
-
-            if(result) {
-                context.commit(DELETE_LANDING_PAGE, { uuid: uuid })
-            }
+        [ACTION_DELETE_LANDING_PAGE]: (context, uuid) => {
+            return new Promise((resolve, reject) => {
+                Api.deleteLandingPage(uuid)
+                    .then(response => {
+                        context.commit(MUTATION_DELETE_LANDING_PAGE, { uuid: uuid })
+                        resolve(response)
+                    })
+                    .catch(error => {
+                        reject(error)
+                    })
+            })
         },
 
         [ACTION_CREATE_LANDING_PAGE]: (context, data) => {
@@ -77,7 +104,7 @@ export default createStore({
                         if(context.state.landingPages == null) {
                             context.dispatch(ACTION_GET_LANDING_PAGES)
                                 .then(() => {
-                                    context.commit(ADD_LANDING_PAGE, { landingPage: response.data })
+                                    context.commit(MUTATION_ADD_LANDING_PAGE, { landingPage: response.data })
                                     resolve(response)
                                 })
                                 .catch(error => {
@@ -85,7 +112,57 @@ export default createStore({
                                 })
                         }
                         else {
-                            context.commit(ADD_LANDING_PAGE, { landingPage: response.data })
+                            context.commit(MUTATION_ADD_LANDING_PAGE, { landingPage: response.data })
+                            resolve(response)
+                        }
+                    })
+                    .catch(error => {
+                        reject(error)
+                    })
+            })
+        },
+
+        [ACTION_UPDATE_DRAFT]: (context, data) => {
+            return new Promise((resolve, reject) => {
+                Api.updateDraft(data.uuid, data.draft)
+                    .then(response => {
+                        if(context.state.landingPages == null) {
+                            context.dispatch(ACTION_GET_LANDING_PAGES)
+                                .then(() => {
+                                    context.commit(MUTATION_UPDATE_LANDING_PAGE, { landingPage: response.data })
+                                    resolve(response)
+                                })
+                                .catch(error => {
+                                    reject(error)
+                                })
+                        }
+                        else {
+                            context.commit(MUTATION_UPDATE_LANDING_PAGE, { landingPage: response.data })
+                            resolve(response)
+                        }
+                    })
+                    .catch(error => {
+                        reject(error)
+                    })
+            })
+        },
+
+        [ACTION_UPDATE_PAGES]: (context, data) => {
+            return new Promise((resolve, reject) => {
+                Api.updatePages(data.uuid, data.pages)
+                    .then(response => {
+                        if(context.state.landingPages == null) {
+                            context.dispatch(ACTION_GET_LANDING_PAGES)
+                                .then(() => {
+                                    context.commit(MUTATION_UPDATE_LANDING_PAGE, { landingPage: response.data })
+                                    resolve(response)
+                                })
+                                .catch(error => {
+                                    reject(error)
+                                })
+                        }
+                        else {
+                            context.commit(MUTATION_UPDATE_LANDING_PAGE, { landingPage: response.data })
                             resolve(response)
                         }
                     })
@@ -107,5 +184,24 @@ export default createStore({
             })
         },
 
+        [ACTION_DISMISS_ALERT]: (context) => {
+            context.commit(MUTATION_SET_ALERT, {
+                alert: {
+                    show: false,
+                    type: null,
+                    message: null
+                }
+            })
+        },
+
+        [ACTION_SHOW_ALERT]: (context, data) => {
+            context.commit(MUTATION_SET_ALERT, {
+                alert: {
+                    show: true,
+                    type: data.type,
+                    message: data.message
+                }
+            })
+        },
     }
 })
