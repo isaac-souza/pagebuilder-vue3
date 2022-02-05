12fbda2449baca87d2c26436e22487939ee51bcd
Isaac Souza <isaacsouza17@gmail.com>
2022-02-05 09:49:14 -0300

    Loading state and emits event with image url

--- src/Components/ImageUploadButton.vue
+++ src/Components/ImageUploadButton.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="">
-        <label class="btn btn-primary">
-            <Icon name="upload" styles="w-5 h-5"/>
-            <span class="ml-1">Upload an image</span>
-            <input @input="upload" type="file" class="hidden"/>
-        </label>
-    </div>
+    <button v-if="processing" disabled class="btn btn-primary disabled loading">
+        Uploading...
+    </button>
+    <label v-else class="btn btn-primary">
+        <Icon name="upload" styles="w-5 h-5"/>
+        <span class="ml-1">Upload an image</span>
+        <input @input="upload" type="file" class="hidden"/>
+    </label>
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
+    import { defineComponent, ref } from 'vue'
     import { useStore } from 'vuex'
 
     import {
@@ -21,32 +22,42 @@
 
     export default defineComponent({
         name: 'ImageUploadButton',
+        emits: ['update:modelValue'],
         components: {
             Icon,
         },
-        setup() {
+        setup(props, context) {
             const store = useStore()
+            const processing = ref(false)
             
             const upload = (event) => {
                 let data = new FormData()
                 data.append('file', event.target.files[0])
 
+                processing.value = true
+
                 store.dispatch(ACTION_UPLOAD_IMAGE, data)
-                    .then(response => {
+                    .then(image => {
+                        context.emit('update:modelValue', image.url)
+
                         store.dispatch(ACTION_SHOW_ALERT, {
                             type: 'success',
                             message: 'Image successfully uploaded!'
                         })
+
+                        processing.value = false
                     })
                     .catch(error => {
                         store.dispatch(ACTION_SHOW_ALERT, {
                             type: 'error',
                             message: 'Error while trying to uploaded the image.'
                         })
+
+                        processing.value = false
                     })
             }
 
-            return { upload }
+            return { upload, processing }
         },
     })
 </script>
