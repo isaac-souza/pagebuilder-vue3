307b638723ffef65644652c9120e24a6dd4aa994
Isaac Souza <isaacsouza17@gmail.com>
2022-02-05 09:48:54 -0300

    New layout/style and emits event with image url

--- src/Components/ImageSelect.vue
+++ src/Components/ImageSelect.vue
@@ -1,23 +1,34 @@
 <template>
-    <div class="form-control">
-        <label class="label -mb-2">
-            <span class="label-text">Imagem da capa</span>
-        </label>
-        <select v-model="url" class="select select-bordered select-sm w-full text-sm font-normal">
-            <option v-for="image in store.state.images" :key="image.uuid" :value="image.url">{{ image.filename }}</option>
-        </select>
-    </div>
+    <details class="px-2 py-1 border border-gray-300 rounded-lg">
+        <summary class="text-sm">
+            Select an image
+        </summary>
+        <div class="grid grid-cols-12">
+            <div v-for="image in store.state.images" :key="image.uuid" class="col-span-12 lg:col-span-6 flex items-center justify-center">
+                <img @click="select(image)" :src="image.thumbUrl" class="py-4 block mx-auto w-20 h-auto">
+            </div>
+        </div>
+        <a href="/app/image-gallery" target="_blank" class="flex items-center justify-center my-4">
+            <span class="text-info text-xs text-center uppercase font-medium mr-1">Open the gallery</span>
+            <Icon name="external-link" styles="w-4 h-4 text-info"/>
+        </a>
+    </details>
 </template>
 
 <script>
-    import { defineComponent, onMounted, computed } from 'vue'
+    import { defineComponent, onMounted } from 'vue'
     import { useStore } from 'vuex'
 
     import { ACTION_GET_IMAGES } from '../Utils/action-types'
 
+    import Icon from '../Components/Icon.vue'
+
     export default defineComponent({
         name: 'ImageSelect',
         emits: ['update:modelValue'],
+        components: {
+            Icon,
+        },
         props: {
             modelValue: {
                 type: String,
@@ -27,18 +38,17 @@
         setup(props, context) {
             const store = useStore()
 
+            const select = (image) => {
+                context.emit('update:modelValue', image.url)
+            }
+
             onMounted(() => {
                 if(store.state.images == null) {
                     store.dispatch(ACTION_GET_IMAGES)
                 }
             })
 
-            const url = computed({
-                get: () => props.modelValue,
-                set: (value) => context.emit('update:modelValue', value),
-            })
-
-            return { store, url }
+            return { store, select }
         }
     })
 </script>
