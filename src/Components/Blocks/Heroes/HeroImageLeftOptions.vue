36660eb7b504eb26b6e73e81826491436399a751
Isaac Souza <isaacsouza17@gmail.com>
2022-02-05 09:49:42 -0300

    Using components the pick and upload image from/to the gallery

--- src/Components/Blocks/Heroes/HeroImageLeftOptions.vue
+++ src/Components/Blocks/Heroes/HeroImageLeftOptions.vue
@@ -24,19 +24,32 @@
             </label>
             <input v-model="block.data.buttonText" type="text" class="input input-bordered input-sm">
         </div>
-        <ImageSelect v-model="block.data.imageUrl"/>
+        <div class="form-control">
+            <label class="label -mb-2">
+                <span class="label-text">Pick from the gallery</span>
+            </label>
+            <ImageSelect v-model="block.data.imageUrl"/>
+        </div>
+        <div class="form-control">
+            <label class="label -mb-2">
+                <span class="label-text">Or upload a new image</span>
+            </label>
+            <ImageUploadButton v-model="block.data.imageUrl" class="btn btn-sm btn-info btn-outline btn-block text-xs font-normal"/>
+        </div>
     </div>
 </template>
 
 <script>
     import { defineComponent, computed } from 'vue'
     import ImageSelect from '../../ImageSelect.vue'
+    import ImageUploadButton from '../../ImageUploadButton.vue'
     
     export default defineComponent({
         name: 'HeroImageLeftOptions',
         emits: ['update:modelValue'],
         components: {
             ImageSelect,
+            ImageUploadButton,
         },
         props: {
             modelValue: {
@@ -45,12 +58,16 @@
             }
         },
         setup(props, context) {
+            const asdf = () => {
+                alert('asdfasd')
+            }
+
             const block = computed({
                 get: () => props.modelValue,
                 set: (value) => context.emit('update:modelValue', value),
             })
 
-            return { block }
+            return { block, asdf }
         },
     })
 </script>
